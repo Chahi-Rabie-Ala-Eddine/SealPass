81d10187f5745b137ff505b7c076685266441af0
eloifabius <eloifabius@gmail.com>
2020-03-11 16:23:54 +0100

    Add files via upload
    Ajouts de commentaire

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import Login from './components/login_page_component';
-import './App.css';
+//point d'entrée de l'applications
 
+import React from 'react';//Importation de React
+import Login from './components/login_page_component';//Importation du composant 'Login'
+import './App.css';//Importation de la feuille de style
+
+//La fonction App est le point d'entré de l'application, il return le bloc principal de l'application.
 function App() {
   return (
+    //Le bloc principal qui charge les différentes composants de l'application
     <div className="App">
+      {/* le composant 'Login' intègre la page de connexion et d'inscription de l'application */}
       <Login />
     </div>
   );
 }
 
+//On Export la fonction principal(Le point d'entré de l'application) de l'application
 export default App;
