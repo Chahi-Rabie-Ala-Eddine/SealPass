009aca0c3a58d0d623b9f74178501ddab2721cd4
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-03-24 15:44:19 +0100

    added form validation on email

--- src/components/change_email_component.jsx
+++ src/components/change_email_component.jsx
@@ -5,20 +5,39 @@ import "../styles/settings_component_style.css";
 class ChangeEmailComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: "email.from@db.ajax" }; /*TODO Get email from server*/
+    this.state = {
+      email: "email.from@db.ajax",
+      emailError: "",
+    }; /*TODO Get email from server*/
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event){
-    this.setState({email: event.target.value});
+  handleChange(event) {
+      this.setState({ email: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("Email Updated" + this.state.email); //TODO change this to good looking stuff
+    const isValid =this.validate();
+    if (isValid){
+      this.setState({emailError : "✓"});
+    }
     event.preventDefault();
   }
 
+  validate(){
+    let emailError = "";
+    var reg = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+    if(!reg.test(this.state.email)){ //TODO REGEX
+      emailError="Invalid email";
+    }
+
+    if(emailError){
+      this.setState({emailError: emailError});
+      return false;
+    }
+    return true;
+  }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
@@ -33,9 +52,14 @@ class ChangeEmailComponent extends Component {
               className="email_input"
               onChange={this.handleChange}
             />
+            <div className="emailError">{this.state.emailError}</div>
           </div>
           <div className="change_email_button_block">
-            <button type="submit" className="change_email_button" value="Submit">
+            <button
+              type="submit"
+              className="change_email_button"
+              value="Submit"
+            >
               Update Email
             </button>
           </div>
