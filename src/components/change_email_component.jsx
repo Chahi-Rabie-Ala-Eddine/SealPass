9a25a946ede5f2544ade871b1fca08eb445b2f8c
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-03-26 15:15:46 +0100

    Added front end password validation

--- src/components/change_email_component.jsx
+++ src/components/change_email_component.jsx
@@ -25,13 +25,12 @@ class ChangeEmailComponent extends Component {
     event.preventDefault();
   }
 
-  validate(){
+  validate(){ //email validation based on 99.99% accurate regExp
     let emailError = "";
-    var reg = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
+    const reg = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])");
     if(!reg.test(this.state.email)){ //TODO REGEX
       emailError="Invalid email";
     }
-
     if(emailError){
       this.setState({emailError: emailError});
       return false;
--- src/components/change_password_component.jsx
+++ src/components/change_password_component.jsx
@@ -5,21 +5,36 @@ import "../styles/settings_component_style.css";
 class ChangePasswordComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { previous_pw: "",
-      new_pw:"",
-      confirm_pw: ""
-    };
-    //this.handleChange = this.handleChange.bind(this);
+    this.state = { previousPwd: "", newPwd: "", confirmPwd: "", pwdError: "" };
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  /*handleChange(event){ //TODO handle multiple fields from event
-    this.setState({password: event.target.value});
-  }*/
+  handleChange(event){
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
 
   handleSubmit(event) {
-    alert("Password Updated"); //TODO change this to good looking stuff
+    const isValid = this.validate();
+    if(isValid) {
+      this.setState({ pwdError: "" });
+    }
+    alert("prevPwd : " + this.state.previousPwd +" newPwd : " +this.state.newPwd+ " confirmPwd " + this.state.confirmPwd );
     event.preventDefault();
+  }
+
+  validate() { //password validation
+    let pwdError = "";
+    if (this.state.newPwd !== this.state.confirmPwd) {
+      pwdError = "Passwords don't match";
+    }
+    if (pwdError) {
+      this.setState({pwdError: pwdError});
+      return false;
+    }
+    return true;
   }
 
   render() {
@@ -29,24 +44,40 @@ class ChangePasswordComponent extends Component {
         {/*change password block*/}
         <div className="pwd_box">
           <h4 className="pwd_title">Change password : </h4>
-          {/*<div className="pwd1">Previous Password</div>{" "}*/}
-          <div>
-            <input type="password" placeholder="Previous password" className="pwd_input_1" />
+          <div className="pwd">
+            <input
+              type="password"
+              placeholder="Previous password"
+              value={this.state.previousPwd}
+              className="pwd_input_1"
+              name="previousPwd"
+              onChange={this.handleChange}
+            />
           </div>
-          {/*<div className="pwd2">New Password</div>{" "}*/}
-          <div>
-            <input type="password" placeholder="New password" className="pwd_input_2" />
+          <div className="pwd">
+            <input
+              type="password"
+              placeholder="New password"
+              className="pwd_input_2"
+              name="newPwd"
+              value={this.state.newPwd}
+              onChange={this.handleChange}
+            />
+            <div className="pwdError">{this.state.pwdError}</div>
           </div>
-          {/*<div className="pwd3">Confirm new Password</div>*/}
-          <div>
-            <input type="password" placeholder="Confirm new password" className="pwd_input_3" />
+          <div className="pwd">
+            <input
+              type="password"
+              placeholder="Confirm new password"
+              className="pwd_input_3"
+              name="confirmPwd"
+              value={this.state.confirmPwd}
+              onChange={this.handleChange}
+            />
+            <div className="pwdError">{this.state.pwdError}</div>
           </div>
           <div className="change_pwd_button_block">
-            <button
-              type="submit"
-              className="change_pwd_button"
-              value = "Submit"
-            >
+            <button type="submit" className="change_pwd_button" value="Submit">
               Update master Password
             </button>
           </div>
