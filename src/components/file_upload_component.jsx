0076bf875fc3dae0e950317bfaefc0670d480822
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-04-16 19:29:42 +0200

    Added working modal for files and folders

--- src/components/file_upload_component.jsx
+++ src/components/file_upload_component.jsx
@@ -1,14 +1,75 @@
 import React, { Component } from "react";
+import Modal from "react-modal";
 import "../styles/cloud_page_component_style.css";
 
 export default class file_upload_component extends Component {
-    //TODO make an overlay with a form for the name
+  constructor(props) {
+    super(props);
+    this.state={
+      showModal: false,
+      selectedFile: null
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.handleUploadFetch = this.handleUploadFetch.bind(this);
+    this.handleUploadRequest = this.handleUploadRequest(this);
+  }
 
+  handleUploadRequest(){//TODO AJAX
+    console.log("uploading file");
+    this.handleCloseModal();
+  }
 
+  handleUploadFetch(event){
+    this.setState({selectedFile:event.target.files[0]})
+  }
+
+  handleCloseModal(){
+    this.setState({showModal: false});
+  }
+
+  handleOpenModal(){
+    this.setState({showModal: true});
+  }
 
   render() {
     return (
-            <button className={"plusButton"}><h1>+</h1></button>
+      <section className={"aroundPlusButton"}>
+        <button className={"plusButton"} onClick={this.handleOpenModal}>
+          <h1>+</h1>
+        </button>
+        <Modal
+          isOpen={this.state.showModal}
+          contentLabel={"onRequestClose"}
+          onRequestClose={this.handleCloseModal}
+          className={"Modal"}
+          overlayClassName={"Overlay"}
+        >
+          <section className={"uploadModal"}>
+            <h2>Upload File</h2>
+            <input
+              className={"fileInput"}
+              type={"file"}
+              name={"file"}
+              onChange={this.handleUploadFetch}
+            />
+            <div className={"buttonRow"}>
+              <button
+                className={"uploadFileButton"}
+                onClick={this.handleUploadRequest}
+              >
+                Upload
+              </button>
+              <button
+                className={"overlayButton"}
+                onClick={this.handleCloseModal}
+              >
+                Cancel
+              </button>
+            </div>
+          </section>
+        </Modal>
+      </section>
     );
   }
 }
--- src/components/newFolderComponent.jsx
+++ src/components/newFolderComponent.jsx
@@ -1,8 +1,73 @@
 import React, { Component } from "react";
 import "../styles/cloud_page_component_style.css";
+import settings_component from "./settings_component";
+import Modal from "react-modal";
 
 export default class newFolderComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      folderName: null,
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.handleUploadRequest = this.handleUploadRequest.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event){
+    this.setState({folderName: event.target.value})
+  }
+
+  handleUploadRequest() {
+    //TODO AJAX
+    console.log("uploading new folder");
+    this.handleCloseModal();
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
   render() {
-    return <button className={"plusButton"}><h1>+</h1></button>;
+    return (
+      <section className={"aroundPlusButton"}>
+        <button className={"plusButton"} onClick={this.handleOpenModal}>
+          <h1>+</h1>
+        </button>
+        <Modal
+          isOpen={this.state.showModal}
+          contentLabel={"onRequestClose"}
+          onRequestClose={this.handleCloseModal}
+          className={"Modal"}
+          overlayClassName={"Overlay"}
+        >
+          <section className={"uploadModal"}>
+            <h2>Create new folder</h2>
+            <input type={"text"} className={"newFolderNameText"} defaultValue={""} onChange={this.handleNameChange}/>
+            <div className={"buttonRow"}>
+              <button
+                className={"createFolderButton"}
+                onClick={this.handleUploadRequest}
+                value={"Submit"}
+              >
+                Create
+              </button>
+              <button
+                className={"overlayButton"}
+                onClick={this.handleCloseModal}
+              >
+                Cancel
+              </button>
+            </div>
+          </section>
+        </Modal>
+      </section>
+    );
   }
 }
