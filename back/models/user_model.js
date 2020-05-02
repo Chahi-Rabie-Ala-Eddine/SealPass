44607e022a05b1ceb650f2cee16d3f083d2e6453
Ala Eddine Rabie Chahi <32160922+ChahiAladeen@users.noreply.github.com>
2020-05-02 15:51:16 +0200

    Delete user_model.js

--- back/models/user_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSignupSchema = mongoose.Schema({
-    fullName: String,
-    email: String,
-    password: String
-});
-
-module.exports = mongoose.model('user_model', userSignupSchema);
