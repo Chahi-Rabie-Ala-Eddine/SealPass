148b3e53c3d7d85f33b0bce41e48b3f5eb80da14
Ala Eddine Rabie Chahi <32160922+ChahiAladeen@users.noreply.github.com>
2020-05-02 15:50:59 +0200

    Delete cloud_model.js

--- back/models/cloud_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cloudFileSchema = mongoose.Schema({
-    fileName: String,
-    fileType: String,
-    filePath: String
-});
-
-module.exports = mongoose.model('cloud_model', cloudFileSchema);
